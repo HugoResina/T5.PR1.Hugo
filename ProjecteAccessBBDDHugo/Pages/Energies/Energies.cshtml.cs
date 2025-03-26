65f4d105f320b91b174915256f616721ee48ba82
Hugo Resina <hugo.resina.7e7@itb.cat>
2025-03-26 11:52:29 +0100

    saving progress

--- ProjecteAccessBBDDHugo/Pages/Energies/Energies.cshtml.cs
+++ ProjecteAccessBBDDHugo/Pages/Energies/Energies.cshtml.cs
@@ -1,16 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjecteAccessBBDDHugo.Data;
+using ProjecteAccessBBDDHugo.Model;
 
 namespace ProjecteAccessBBDDHugo.Pages.Energies
 {
 
     public class EnergiesModel : PageModel
     {
-        public void OnGet()
-        {
+		public IList<Simulation> Energies { get; set; }
+		private readonly ApplicationDbContext _context;
+		public EnergiesModel(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public void OnGet()
+		{
 
-        }
+			Energies = _context.Simulations.ToList();
+			
+
+		}
 
 
-    }
+
+	}
 }
